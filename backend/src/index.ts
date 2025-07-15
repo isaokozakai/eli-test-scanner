40f51de19b0183bd7b58c1ebdfc85a601b350fce
Isao Kozakai <isaokozakai@gmail.com>
2025-07-15 12:27:17 -0400

    initial commit: project setup

---
+++ backend/src/index.ts
@@ -0,0 +1,6 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (_, res) => res.send("Backend is running"));
+app.listen(3000, () => console.log("Server running on port 3000"));
