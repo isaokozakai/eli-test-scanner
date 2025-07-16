48a5b6ee98ffee1e77fa63a717c28b4ba7492b4e
Isao Kozakai <isaokozakai@gmail.com>
2025-07-15 21:45:31 -0400

    setup: reset mobile folder to not use Expo

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera, useCameraDevice } from 'react-native-vision-camera';
+import { useEffect, useState } from 'react';
+import { Text, StyleSheet } from 'react-native';
 
-export default function App() {
+export default function CameraScreen() {
+  const [hasPermission, setHasPermission] = useState(false);
+  const device = useCameraDevice('back');
+
+  useEffect(() => {
+    (async () => {
+      const permission = (await Camera.requestCameraPermission()) as
+        | 'authorized'
+        | 'denied'
+        | 'not-determined'
+        | 'restricted';
+      setHasPermission(permission === 'authorized');
+    })();
+  }, []);
+
+  if (device == null) return <Text style={styles.message}>Loading...</Text>;
+  if (!hasPermission)
+    return <Text style={styles.message}>No camera permission</Text>;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Camera
+      style={styles.camera}
+      device={device}
+      isActive={true}
+      photo={true}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  camera: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  message: {
+    flex: 1,
+    textAlign: 'center',
+    textAlignVertical: 'center', // Android vertical center
+    fontSize: 18,
+    color: '#333',
   },
 });
